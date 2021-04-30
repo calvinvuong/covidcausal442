a94c333f475eed235e1b542c6d535fc28292d9c4
Zane Varner <zanevarner0@gmail.com>
2021-04-29 21:45:39 -0400

    Added bootstrapping module for computing effect estimates and confidence intervals.

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,3 +1,4 @@
 from .ip_weighting import compute_effects_ip_weighting
 from .standardization import compute_effects_standardization
 from .preprocessing import standardize_data
+from .bootstrapping import bootstrap, compute_effect_measure
